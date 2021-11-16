7770817b69d3865cb3d6d0fdc61f0719ba737241
Intravision\Igor Kozhin <Igor.Kozhin@intravision.ru>
2021-11-16 11:09:47 +1000

    #Init

---
+++ Common/Models/Xlsx/XlsxColumn.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Common.Extensions
+{
+    public class XlsxColumn
+    {
+        public string DisplayName { get; set; }
+        public Type ColumnType { get; set; }
+        public int? Order { get; set; }
+    }
+}
---
+++ WebAppAspNetMvcExportExcel/Controllers/GenresController.cs
@@ -0,0 +1,100 @@
+﻿using Common.Extensions;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using WebAppAspNetMvcExportExcel.Models;
+
+namespace WebAppAspNetMvcExportExcel.Controllers
+{
+    public class GenresController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var db = new LibraryContext();
+            var genres = db.Genres.ToList();
+
+            return View(genres);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var genre = new Genre();
+            return View(genre);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Genre model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var db = new LibraryContext();                      
+
+            db.Genres.Add(model);
+            db.SaveChanges();
+
+            return RedirectPermanent("/Genres/Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var db = new LibraryContext();
+            var genre = db.Genres.FirstOrDefault(x => x.Id == id);
+            if (genre == null)
+                return RedirectPermanent("/Genres/Index");
+
+            db.Genres.Remove(genre);
+            db.SaveChanges();
+
+            return RedirectPermanent("/Genres/Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new LibraryContext();
+            var genre = db.Genres.FirstOrDefault(x => x.Id == id);
+            if (genre == null)
+                return RedirectPermanent("/Genres/Index");
+
+            return View(genre);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Genre model)
+        {
+            var db = new LibraryContext();
+            var genre = db.Genres.FirstOrDefault(x => x.Id == model.Id);
+            if (genre == null)
+                ModelState.AddModelError("Id", "Жанр не найден");
+
+            if (!ModelState.IsValid)
+                return View(model);
+
+            MappingGenre(model, genre);
+
+            db.Entry(genre).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectPermanent("/Genres/Index");
+        }
+
+        private void MappingGenre(Genre sourse, Genre destination)
+        {
+            destination.Name = sourse.Name;
+        }
+
+        [HttpGet]
+        public ActionResult GetXlsx()
+        {
+            var db = new LibraryContext();
+            var xlsx = db.Genres.ToXlsx();
+
+            return File(xlsx.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Data.xlsx");
+        }
+    }
+}
